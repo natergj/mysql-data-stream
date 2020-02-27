3c9957ed838a9c241b76af6edf3680b0198e69b9
Nater Jorde <njorde@propointglobal.com>
2020-02-27 08:27:45 -0600

    small code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import mysql from "mysql2";
 import { createWriteStream } from "fs";
 
-const connection =  mysql.createConnection({
+const connection = mysql.createConnection({
   host: "127.0.0.1",
+  port: "13306",
   user: "dev_user",
-  password: "dev_password"
+  password: "dev_password",
 });
 
 function writeEmployees() {
   return new Promise((resolve, reject) => {
     const outFile = createWriteStream("./output.csv");
     connection.query("use employees");
-    const query = connection.execute('SELECT * FROM `employees`');
+    const query = connection.execute("SELECT * FROM `employees`");
 
     query.on("result", row => {
       const csvLine: string[] = [];
